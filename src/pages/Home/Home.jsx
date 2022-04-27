b23b4fb6760981cda1985c92edd4abd4b3f9e193
nyomanagil1 <nyoman44@gmail.com>
2022-04-27 21:53:37 +0700

    add logout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,12 +5,26 @@ function Home() {
   const token = localStorage.getItem('token');
   const navigate = useNavigate();
 
+  function handleLogout() {
+    localStorage.clear();
+    navigate('/login');
+  }
+
   useEffect(() => {
     if (!token) {
       navigate('/login');
     }
   }, []);
-  return <div>Home</div>;
+  return (
+    <>
+      <button
+        class="inline-block px-6 py-2 border-2 border-green-500 text-green-500 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+        onClick={() => handleLogout()}
+      >
+        LOGOUT
+      </button>
+    </>
+  );
 }
 
 export default Home;
